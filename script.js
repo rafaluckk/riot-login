3dc765abcaa265bfd7a5338785557777832e261f
rafaluckk <rafael2021516@gmail.com>
2023-04-03 11:00:25 -0300

    arquivos inúteis

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputs = document.querySelectorAll("input");
 const button = document.querySelector('.login__button');
 
+
+
 const handleFocus = ({ target }) => {
   const span = target.previousElementSibling;
   span.classList.add("span-active");
